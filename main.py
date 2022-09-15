4b0eba2067f80014dab094471463ab6a88204c02
jose3889 <jose3889@hotmail.es>
2022-09-15 07:48:25 -0300

    f

--- main.py
+++ main.py
@@ -33,20 +33,50 @@ async def piloto():
 # se crea el get de fastAPI que devuelve el circuito más recorrido
 
 @app.get("/circuito/")
-async def query():
-    return crud.circuito()
+async def circuito():
+    query_circuito ='''select distinct c.name as circuito, count(r.circuitId) as CircuitoMasRecorrido
+                        from circuit c
+                        join race r
+                        on r.circuitId=c.circuitId
+                        group by c.name
+                        order by CircuitoMasRecorrido DESC
+                        LIMIT 1;'''
+    df = pd.read_sql(query_circuito, engine)
+    circuito=df.iloc[0]['circuito']
+    return {"El circuito más recorrido es:": circuito}
    
 
 # se crea el get de fastAPI que devuelve el piloto más ganador 
 # con constructor americano o britanico
 
 @app.get("/piloto_ganador/")
-async def query():
-    return crud.piloto_ganador()
+async def piloto_ganador():
+    query_piloto ='''select r.surname as piloto, sum(r.points) as CantPuntosTotales
+                    from (select c.constructorId
+                            from constructor c
+                            where c.nationality in ('British','American')) as c
+                    join (select r.driverId, d.surname,  r.constructorId, r.points
+                            from driver d
+                            join result r
+                            on d.driverId = r.driverId) as r
+                    on r.constructorId=c.constructorId
+                    group by r.surname
+                    order by CantPuntosTotales DESC
+                    LIMIT 1;'''
+    df = pd.read_sql(query_piloto, engine)
+    piloto=df.iloc[0]['piloto']
+    return {"El piloto con mayor cantidad de puntos con constructor de ameriacano o britanico es:": piloto}
     
 # se crea el get de fastAPI que devuelve el año con mas carreras
 
 @app.get("/year/")
-async def query():
-    return crud.year()
+async def year():
+    query_year ='''select year ,count(raceId) as carreras
+                    from race
+                    group by year
+                    order by carreras DESC
+                    LIMIT 1;'''
+    df = pd.read_sql(query_year, engine)
+    year=df.iloc[0]['year']
+    return {"El año con más carreras es:": year}
    
