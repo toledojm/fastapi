22a488cb66fad920fdb9a72e4c22a3360e71c454
jose3889 <jose3889@hotmail.es>
2022-09-15 08:37:34 -0300

    f

--- database.py
+++ database.py
@@ -5,26 +5,26 @@ from models import *
 
 # se realiza la conexion a base de datos jwsDB MySQL
 
-SQLALCHEMY_DATABASE_URL = "mysql://prs679z1zrvo3g14:g51vxn3imv6mx9i6@cwe1u6tjijexv3r6.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/hp3gqgswj6ocf8l3"
+SQLALCHEMY_DATABASE_URL = "mysql://c3zpbaqh6klc7gdq:o0x2p2l6asmydogd@cwe1u6tjijexv3r6.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/wwhlk06iufgyn4ua"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
-#holla
+
 # se ingesta los datos a tablas a un database de jwsDB MySQL
 # se crea claves primarias y foraneas para relacionar las tablas creadas
 
-with engine.connect() as conn, conn.begin():
-    df_circuits.to_sql('circuit', conn, if_exists='replace', index=False)
-    df_constructors.to_sql('constructor', conn, if_exists='replace', index=False)
-    df_drivers.to_sql('driver', conn, if_exists='replace', index=False)
-    df_results.to_sql('result', conn, if_exists='replace', index=False)
-    df_races.to_sql('race', conn, if_exists='replace', index=False)
-    conn.execute('''ALTER TABLE circuit ADD PRIMARY KEY(circuitId);''')
-    conn.execute('''ALTER TABLE constructor ADD PRIMARY KEY(constructorId);''')
-    conn.execute('''ALTER TABLE driver ADD PRIMARY KEY(driverId);''')
-    conn.execute('''ALTER TABLE result ADD PRIMARY KEY(resultId);''')
-    conn.execute('''ALTER TABLE race ADD PRIMARY KEY(raceId);''')
-    conn.execute('''ALTER TABLE race ADD foreign key (circuitId) references circuit(circuitId);''')
-    conn.execute('''ALTER TABLE result ADD foreign key (raceId) references race(raceId);''')
-    conn.execute('''ALTER TABLE result ADD foreign key (driverId) references driver(driverId);''')
-    conn.execute('''ALTER TABLE result ADD foreign key (constructorId) references constructor(constructorId);''')
+with engine.connect() as con, con.begin():
+    df_circuits.to_sql('circuit', con, if_exists='replace', index=False)
+    df_constructors.to_sql('constructor', con, if_exists='replace', index=False)
+    df_drivers.to_sql('driver', con, if_exists='replace', index=False)
+    df_results.to_sql('result', con, if_exists='replace', index=False)
+    df_races.to_sql('race', con, if_exists='replace', index=False)
+    con.execute('''ALTER TABLE circuit ADD PRIMARY KEY(circuitId);''')
+    con.execute('''ALTER TABLE constructor ADD PRIMARY KEY(constructorId);''')
+    con.execute('''ALTER TABLE driver ADD PRIMARY KEY(driverId);''')
+    con.execute('''ALTER TABLE result ADD PRIMARY KEY(resultId);''')
+    con.execute('''ALTER TABLE race ADD PRIMARY KEY(raceId);''')
+    con.execute('''ALTER TABLE race ADD foreign key (circuitId) references circuit(circuitId);''')
+    con.execute('''ALTER TABLE result ADD foreign key (raceId) references race(raceId);''')
+    con.execute('''ALTER TABLE result ADD foreign key (driverId) references driver(driverId);''')
+    con.execute('''ALTER TABLE result ADD foreign key (constructorId) references constructor(constructorId);''')
  
