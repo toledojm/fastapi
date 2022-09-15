ffd416a82b3f0ad6bf43ee4bc17992626eaba922
jose toledo <104653070+toledojm@users.noreply.github.com>
2022-09-15 11:15:41 -0300

    Update database.py

--- database.py
+++ database.py
@@ -13,18 +13,22 @@ engine = create_engine(SQLALCHEMY_DATABASE_URL)
 # se crea claves primarias y foraneas para relacionar las tablas creadas
 
 with engine.connect() as con, con.begin():
-    df_circuits.to_sql('circuit', con, if_exists='replace', index=False)
-    df_constructors.to_sql('constructor', con, if_exists='replace', index=False)
-    df_drivers.to_sql('driver', con, if_exists='replace', index=False)
-    df_results.to_sql('result', con, if_exists='replace', index=False)
-    df_races.to_sql('race', con, if_exists='replace', index=False)
-    con.execute('''ALTER TABLE circuit ADD PRIMARY KEY(circuitId);''')
-    con.execute('''ALTER TABLE constructor ADD PRIMARY KEY(constructorId);''')
-    con.execute('''ALTER TABLE driver ADD PRIMARY KEY(driverId);''')
-    con.execute('''ALTER TABLE result ADD PRIMARY KEY(resultId);''')
-    con.execute('''ALTER TABLE race ADD PRIMARY KEY(raceId);''')
-    con.execute('''ALTER TABLE race ADD foreign key (circuitId) references circuit(circuitId);''')
-    con.execute('''ALTER TABLE result ADD foreign key (raceId) references race(raceId);''')
-    con.execute('''ALTER TABLE result ADD foreign key (driverId) references driver(driverId);''')
-    con.execute('''ALTER TABLE result ADD foreign key (constructorId) references constructor(constructorId);''')
+    try:
+        df_races.to_sql('race', con, if_exists='fail', index=False)
+        df_drivers.to_sql('driver', con, if_exists='fail', index=False)
+        df_results.to_sql('result', con, if_exists='fail', index=False)
+        df_constructors.to_sql('constructor', con, if_exists='fail', index=False)
+        df_circuits.to_sql('circuit', con, if_exists='fail', index=False)
+        con.execute('''ALTER TABLE circuit ADD PRIMARY KEY(circuitId);''')
+        con.execute('''ALTER TABLE constructor ADD PRIMARY KEY(constructorId);''')
+        con.execute('''ALTER TABLE driver ADD PRIMARY KEY(driverId);''')
+        con.execute('''ALTER TABLE result ADD PRIMARY KEY(resultId);''')
+        con.execute('''ALTER TABLE race ADD PRIMARY KEY(raceId);''')
+        con.execute('''ALTER TABLE race ADD foreign key (circuitId) references circuit(circuitId);''')
+        con.execute('''ALTER TABLE result ADD foreign key (raceId) references race(raceId);''')
+        con.execute('''ALTER TABLE result ADD foreign key (driverId) references driver(driverId);''')
+        con.execute('''ALTER TABLE result ADD foreign key (constructorId) references constructor(constructorId);''')
+    except ValueError:
+        pass
+
  
